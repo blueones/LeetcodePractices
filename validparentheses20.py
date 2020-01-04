e2e5d11b599b99b18db683bd31ddf8ef884ee531
blueones <yiq.shang@gmail.com>
2020-01-03 20:50:17 -0800

    Jan 3rd finished 1st,valid parentheses 20 better solution

--- validparentheses20.py
+++ validparentheses20.py
@@ -22,5 +22,17 @@ class Solution:
         return False
 class Solution2:
     def isvalid(self,s):
-        pass
+        parenDictionary={'}':'{',']':'[',')':'('}
+        stackparen=[]
+        for paren in s:
+            if paren in parenDictionary:
+                if stackparen==[]:
+                    return False
+                else:
+                    if parenDictionary[paren]!=stackparen.pop(-1):
+                        return False
+            else:
+                stackparen.append(paren)
+        if stackparen==[]:
+            return True
 print(Solution().isValid("[]")))
