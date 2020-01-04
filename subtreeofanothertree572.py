42005fa04bb7a6c3667a66372d8dd9c478aa29be
blueones <yiq.shang@gmail.com>
2020-01-04 10:08:00 -0800

    Jan 4th, better solution self for 572

--- subtreeofanothertree572.py
+++ subtreeofanothertree572.py
@@ -13,59 +13,36 @@ class Solution:
         def comparetrees(s,t):
             if s==t==None:
                 return True
-            elif t==None and s!=None:
-                return False
-            elif t!=None and s==None:
-                return False
-            else:
+            elif s and t:
                 if s.val==t.val:
-                    #print("s is ",s.val,"t is ",t.val)
-                    leftR=rightR=False
-                    leftR=comparetrees(s.left,t.left)
-                    rightR=comparetrees(s.right,t.right)
-                    #print(leftR and rightR, "return for this level")
-                    return leftR and rightR
+                    return comparetrees(s.left,t.left) and comparetrees(s.right,t.right)
             return False
 
-        
-        if s.val==t.val:
-            if comparetrees(s,t):
-                return True
-            else:
-                leftR=rightR=False
-            if s.left:
-                leftR= self.isSubtree(s.left,t)
-                print("should've been here")
-                if leftR:
-                    return leftR
-            if s.right:
-                rightR= self.isSubtree(s.right,t)
-                if rightR:
-                    return rightR
-        else: 
-            print("here?")
-            leftR=rightR=False
-            if s.left:
-                leftR= self.isSubtree(s.left,t)
-                print("should've been here")
-                if leftR:
-                    return leftR
-            if s.right:
-                rightR= self.isSubtree(s.right,t)
-                if rightR:
-                    return rightR
-        return False
+        if s and t:
+            if s.val==t.val:
+                if comparetrees(s,t):
+                    return True
+                else:
+                    return self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
+            else: 
+                return self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
+        elif not (s or t):
+            return True
+        else:
+            return False
+                
 
-# sunnynode=TreeNode(3)
-# sunnynode.left=TreeNode(4)
-# sunnynode.right=TreeNode(5)
-# sunnynode.left.left=TreeNode(1)
-# sunnynode.left.right=TreeNode(2)
-# sunny=TreeNode(4)
-# sunny.left=TreeNode(1)
-# sunny.right=TreeNode(2)
+sunnynode=TreeNode(3)
+sunnynode.left=TreeNode(4)
+sunnynode.right=TreeNode(5)
+sunnynode.left.left=TreeNode(1)
+sunnynode.left.right=TreeNode(2)
+sunnynode.left.left.left=TreeNode(0)
+sunny=TreeNode(4)
+sunny.left=TreeNode(1)
+sunny.right=TreeNode(2)
  
-sunnynode=TreeNode(1)
+'''sunnynode=TreeNode(1)
 sunnynode.left=TreeNode(1)
-sunny=TreeNode(1)
+sunny=TreeNode(1)'''
 print(Solution().isSubtree(sunnynode,sunny))
