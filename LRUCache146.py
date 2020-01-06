11b6467e7675ff51eacbe847fa0fc5382b152428
blueones <yiq.shang@gmail.com>
2020-01-05 22:29:01 -0800

    Jan 5th LRUCache cheating solution

--- LRUCache146.py
+++ LRUCache146.py
@@ -1,12 +1,39 @@
+from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        
+        self.length=0
+        self.cap=capacity
+        self.cache=OrderedDict()
 
     def get(self, key: int) -> int:
+        if self.length==0 or key not in self.cache:
+            return -1
+        else:
+            value=self.cache[key]
+            del self.cache[key]
+            self.length-=1
+            self.put(key,value)
+            return self.cache[key]
         
 
     def put(self, key: int, value: int) -> None:
+        if self.length==self.cap:
+            if key in self.cache:
+                del self.cache[key]
+                self.cache[key]=value
+            else:
+                self.cache.popitem(last=False)
+                self.cache[key]=value
+        else:    
+            if key not in self.cache:
+                self.length+=1
+                self.cache[key]=value
+            else:
+                del self.cache[key]
+                self.cache[key]=value
+            
+        print("putting key ",key, "now dic is ", self.cache)
         
 
 
@@ -14,3 +41,10 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+obj=LRUCache(2)
+obj.put(2,1)
+obj.put(1,1)
+obj.put(2,3)
+obj.put(4,1)
+obj.get(1)
+obj.get(2)
--- copylistwithrandompointer138.py
+++ copylistwithrandompointer138.py
@@ -9,15 +9,29 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         #copy one by one, stop when for a node, the next is null
-        def buildNode(node,index):
-            copyNode=Node(node.val)
-            copyNode.next=
+        def copynext(head2):
+            dictO={}
+            before=None
+            returnh=before
+            head1=head2
+            num=0
+            while head1!=None:
+                headc=Node(head1.val)
+                dictO[num]=head1.random
+                num+=1
+                if head1.random in listcreated:
+                if head.next==None:
+                    before.next=headc
+                    headc.next=None
+                elif head.next!=None:
+                    before.next=headc
+                    before=headc
+                    head1=head1.next
+            for node in dictO:
 
-        if head==None:
-            return None
-        else:
-            copyNode=Node(head.val)
-            copyNode.random=self.copyRandomList(head.random)
-            copyNode.next=self.copyRandomList(head.next)
-            return copyNode
+            return returnh.next
+        newhead=copynext(head)
+
+
+
         
