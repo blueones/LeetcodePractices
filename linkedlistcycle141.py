e59f97f290fba2ac4d6080e81b0defbef99809e4
blueones <yiq.shang@gmail.com>
2020-01-06 20:48:57 -0800

    Jan 6th two pointer solving linked list cycle

---
+++ linkedlistcycle141.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution1:
+    #use hashtable to remember visited nodes
+    def hasCycle(self, head: ListNode) -> bool:
+        visited=list()
+        while head!=None:
+            if head in visited:
+                return True
+            else:
+                visited.append(head)
+                head=head.next
+        return False
+
+        
+
+class Solution2:
+    #two pointers, a fast runner and a slower runner. 
+    # if they are running on a cycle, fast runner will eventually catch up with slower runner again. 
+    def hasCycle(self, head: ListNode) -> bool:
+        if head==None or head.next==None:
+            return False
+        fastR=head
+        slowR=head
+        while fastR!=None and fastR.next!=None:
+            fastR=fastR.next.next
+            slowR=slowR.next
+            if fastR==slowR:
+                return True
+        return False
+    
