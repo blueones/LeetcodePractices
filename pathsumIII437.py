e49c6f52f1a0b217dea840b520c0fe5bd5a0f55e
blueones <yiq.shang@gmail.com>
2020-01-25 17:15:38 -0800

    path sum iii Jan 25th

--- pathsumIII437.py
+++ pathsumIII437.py
@@ -7,4 +7,33 @@
 
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> int:
+        #brute force. go thru each node, and for that node leading subtree, see if sum is met. 
+        self.resultNum = 0
+
+
+        def dfs(root):
+            #traverse the tree to check each node
+            testSum(root,sum)
+            if root.left:
+                dfs(root.left)
+            if root.right:
+                dfs(root.right)
+        def testSum(node, currentSum):
+            #check for this node, if there is any path with the target sum
+            if node != None:
+                if node.val == currentSum:
+                    self.resultNum +=1
+                if node.left:
+                    testSum(node.left, currentSum - node.val)
+                if node.right:
+                    testSum(node.right,currentSum - node.val)
+
+
+
+
+        if root == None:
+            return 0
+
         
+        dfs(root)
+        return self.resultNum
