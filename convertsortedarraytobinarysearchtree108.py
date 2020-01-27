3df1b703d08aed4f3d49c96e0b42ae43097d5d89
blueones <yiq.shang@gmail.com>
2020-01-26 17:09:59 -0800

    Jan 26th, divide and conquer practice. Be careful with margins

--- convertsortedarraytobinarysearchtree108.py
+++ convertsortedarraytobinarysearchtree108.py
@@ -5,10 +5,11 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution1:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         # divide and conquer
         #recursion
+        #generate new list for every recursion. Not ideal for space and time complexity.
         
         lenS = len(nums)
         if lenS == 0:
@@ -18,3 +19,33 @@ class Solution:
         headNode.left = self.sortedArrayToBST(nums[:midIndex])
         headNode.right = self.sortedArrayToBST(nums[midIndex+1:])
         return headNode
+class Solution2:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        #only access one list. better than solution1 in time and space complexity
+        #include left margin but left out right margin.
+        lenS = len(nums)
+        def dfs(left, right):
+            if left == right:
+                return None
+            midIndex = (left+right)//2
+            headNode = TreeNode(nums[midIndex])
+            if midIndex-1>= left:
+                headNode.left = dfs(left,midIndex)
+            headNode.right = dfs(midIndex+1,right)
+            return headNode
+        return dfs(0,lenS)
+class Solution3:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        #only access one list. better than solution1 in time and space complexity
+        #include left and right margins. so left can be equal to right. 
+        lenS = len(nums)
+        def dfs(left, right):
+            if left > right: #included left and right margins, so left can be equal to right
+                return None
+            midIndex = (left+right)//2
+            headNode = TreeNode(nums[midIndex])
+            if midIndex-1>= left:
+                headNode.left = dfs(left,midIndex-1)
+            headNode.right = dfs(midIndex+1,right)
+            return headNode
+        return dfs(0,lenS-1)
