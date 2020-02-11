6faacd3512f4e5e52dcd34341fc6de119128274c
blueones <yiq.shang@gmail.com>
2020-02-10 19:38:13 -0800

    better time complexity for implementing queue

--- implementingqueueusingstacks232.py
+++ implementingqueueusingstacks232.py
@@ -30,62 +30,49 @@ class MyQueue:
         return queueFront
         
 class MyQueue1:
-
+    #GOOD PERFORMANCE COMPARED WITH MYQUEUE 1. USE A STACK FOR INPUTTING AND ANOTHER STACK FOR CACHE(to pop from. refill from store stack when empty.)
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.stackStore = []
+        self.stackCache = []
+        self.length = 0
         
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-        
+        self.stackStore.append(x)
+        self.length += 1
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
+        if self.stackCache == []:
+            while self.stackStore != []:           
+                self.stackCache.append(self.stackStore.pop(-1))
+        self.length -= 1
+        return self.stackCache.pop(-1)
         
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if self.stackCache == []:
+            while self.stackStore != []:           
+                self.stackCache.append(self.stackStore.pop(-1))
+        return self.stackCache[-1]
         
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-             
-
-        
-
-    def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        tempStack = []
-        while self.stack:
-            tempStack.append(self.stack.pop(-1))
-        queueFront = tempStack.pop(-1)
-        tempStack.append(queueFront)
-        while tempStack:
-            self.stack.append(tempStack.pop(-1))
-        return queueFront
-        
-
-    def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        if self.length == 0:
-            return True
-        return False
-        
-
+        return self.length == 0 
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
