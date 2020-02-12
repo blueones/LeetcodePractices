3009354770c7de5853306191e824ad165ae7e5ed
blueones <yiq.shang@gmail.com>
2020-02-11 22:04:09 -0800

    binary search tree

--- insertintoabinarysearchtree701.py
+++ insertintoabinarysearchtree701.py
@@ -21,3 +21,17 @@ class Solution:
                     recursionsearch(node.left,val)
         recursionsearch(root,val)
         return root
+class Solution1:
+    def insertIntoBST(self,root,val):
+        #doesn't really need a helper function here.
+        if root.val < val:
+            if root.right == None:
+                root.right = TreeNode(val)
+            else:
+                self.insertIntoBST(root.right,val)
+        elif root.val >val:
+            if root.left == None:
+                root.left = TreeNode(val)
+            else:
+                self.insertIntoBST(root.left,val)
+        return root
