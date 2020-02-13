fe85b67665554bd8099709a4ba004e4db8538018
blueones <yiq.shang@gmail.com>
2020-02-12 18:16:39 -0800

    construct tree using preorder and inorder

--- constructbinarytreefrompreorderandinordertraversal105.py
+++ constructbinarytreefrompreorderandinordertraversal105.py
@@ -19,3 +19,26 @@ class Solution:
             node.right = dfs(inorder.index(node.val)+1, in_right)
             return node
         return dfs(0, len(inorder))
+class Solution1:
+    def buildTree(self,preorder, inorder):
+        # think hard about how preorder and inorder is being created. consider left and right as a big chunk of things.
+        #  (left chunk) root (right chunk) inorder
+        #  root (left chunk) (right chunk) preorder
+        #  this solution use the location of root in inorder, to find out the length of left chunk and so the length of right chunk. 
+        #  then recurse. 
+        self.preorder = preorder
+        self.inorder = inorder
+        def build(preB, preE, inB, inE):
+            if preB == preE:
+                return None
+            currentV = self.preorder[preB]
+            node = TreeNode(currentV)
+            if preB + 1 == preE:
+                return node
+            indexNin = self.inorder.index(currentV)
+            node.left = build(preB+1, preB+ indexNin- inB+1, inB, indexNin )
+            node.right = build(preB + indexNin - inB +1, preE, indexNin+1, inE)
+            return node
+        lenL = len(preorder)
+        return build(0, lenL, 0, lenL)
+
