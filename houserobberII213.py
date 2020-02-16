6dc58a81c67355db976a9160aaf4e86c68e2c07c
blueones <yiq.shang@gmail.com>
2020-02-16 09:20:17 -0800

    house robber II 213

--- houserobberII213.py
+++ houserobberII213.py
@@ -1,3 +1,23 @@
+
+'''
+Since every house is either robbed or not robbed and at least half of the houses are not robbed,
+ the solution is simply the larger of two cases with consecutive houses, 
+ i.e. house i not robbed, break the circle, solve it, or house i + 1 not robbed. 
+ Hence, the following solution.
+ I chose i = n and i + 1 = 0 for simpler coding. But, you can choose whichever two consecutive ones.
+
+ Great solution. It took me a while to figure out why this is logically correct.
+  At the first glance, I think the perfect way to split the problem is 1. not rob the 1st house; 
+  2. rob 1st house, because 1 and 2 won't have any intersection (code below follows this idea and beats 100%). 
+  However, the way this solution splits this problem is
+   1. not rob the 1st house; 2. not rob the last house.
+    As you can see, the second statement of these two split strategies are different 
+    and they are not logically equal because "not rob the last house" means 
+    you can choose to rob the 1st house or not. Then why it is still correct? 
+    It is because the 2nd statement from the 2nd strategy contains the 2nd statement from the 1st strategy. 
+    In other words, the 2nd set has some overlap with the 1st set in the 2nd strategy. 
+    Since our goal is only to find the max, it is okay to include some overlap.
+'''
 class Solution:
     def rob(self, nums):
         lenN = len(nums)
@@ -76,3 +96,14 @@ class Solution1:
             self.maxR = max(self.robberM[lenN-1][0], self.robberM[lenN-1][1])
         return self.maxR
 Solution1().rob([2,2,4,3,2,5])
+class Solution2:
+    def rob(self, nums):
+        lenN = len(nums)
+        self.robberM = [(0,0,0,0) for i in range(lenN)]
+        #self.maxR = 0 # original self.maxR. in this question could just be self.maxR = max(self.robberM[0][0],self.robberM[0][1]). to deal with test case where lenN == 1
+        if lenN == 0:
+            return 0
+        #first item is when this item is included, and include first item nums[0], the max
+        #second item is when this item is not included, and include the first item nums[0], the max
+        #third item is when this item is included,, and not include the first item nums[0],
+        #forth item is when this item is not included,, and not include the first item nums[0],
