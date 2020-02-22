1bb4134f0378ef8346a3a7dba756199368a17064
blueones <yiq.shang@gmail.com>
2020-02-22 12:56:10 -0800

    easily made mistake 784. backtrack question.

--- lettercasepermutation784.py
+++ lettercasepermutation784.py
@@ -1,5 +1,5 @@
 class Solution:
-    def letterCasePermutation(self, S: str) -> List[str]:
+    def letterCasePermutation(self, S) :
         self.resultL = []
         lenS = len(S)
         def backtracking(index,current):
@@ -19,3 +19,23 @@ class Solution:
                     backtracking(index+1,current)
         backtracking(0,"")
         return self.resultL
+class Solution1:
+    def letterCasePermutation(self,S):
+        if S:
+            if S[0].isalpha():
+                currentL = [S[0].upper(), S[0].lower()]
+            else:
+                currentL = [S[0]]
+            res = []
+            for item in self.letterCasePermutation(S[1:]):
+                for stringC in currentL:
+                    res.append(stringC + item)
+            #wrong answer: think about it's not what you want. easily made mistake
+            # for item in self.letterCasePermutation(s[1:]):
+            #     for i in range(len(currentL)):
+            #         currentL[i] += item
+            return res
+        else:
+            return [""]#very easily made mistake here. 
+
+Solution1().letterCasePermutation("a1b1")           
