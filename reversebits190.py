d506b043a9107c8994aaba5a3638702d1ccd2c05
blueones <yiq.shang@gmail.com>
2020-03-13 20:47:43 -0700

    reversebits

--- reversebits190.py
+++ reversebits190.py
@@ -1,2 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        #how would you optimize it
+        resultN = 0
+        for i in range(32):
+            n%2 = lastDigit
+            n = n//2
+            resultN = resultN*2+lastDigit
+        return resultN
