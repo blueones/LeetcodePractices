9a3d0e0ebc4d2699f043e2c1e9fb408a445d32c8
blueones <yiq.shang@gmail.com>
2020-03-14 10:08:48 -0700

    two pointer solution for 4 sum

--- 4sum18.py
+++ 4sum18.py
@@ -24,4 +24,37 @@ class Solution:
                     else:
                         setH.add(currentV)
         return ans
-Solution().fourSum([0,0,0,0],0)
+# Solution().fourSum([0,0,0,0],0)
+class Solution1:
+    def fourSum(self,nums,target):
+        lenN = len(nums)
+        nums.sort()
+        ans = []
+        for i in range(lenN):
+            if i!=0 and nums[i]==nums[i-1]:
+                continue
+            currentI = nums[i]
+            iTarget = target - currentI
+            for j in range(i+1,lenN):
+                if j != i+1 and nums[j]== nums[j-1]:
+                    continue
+                currentJ = nums[j]
+                jTarget = iTarget - currentJ
+                leftM = j+1
+                rightM = lenN-1
+                while leftM<rightM:
+                    if nums[leftM]+nums[rightM]==jTarget:
+                        ans.append([currentI,currentJ,nums[leftM],nums[rightM]])
+                        leftM+=1
+                        rightM-=1
+                        while leftM-1>j and leftM<lenN and nums[leftM-1]==nums[leftM]: #leftM <lenN easy to forget
+                            leftM+=1
+                        while rightM+1<lenN and rightM>j and nums[rightM+1] == nums[rightM]:# rightM>j easy to forget
+                            rightM-=1
+                    elif nums[leftM]+nums[rightM] > jTarget:
+                        rightM -= 1
+                    elif nums[leftM]+nums[rightM] < jTarget:
+                        leftM += 1
+        return ans
+
+Solution1().fourSum([0,0,0,0],0)
