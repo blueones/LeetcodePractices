ac52105a4c5caf68312cc7d25a34eb3a0e714a0c
blueones <yiq.shang@gmail.com>
2020-03-25 18:49:37 -0700

    March 24th 1st

--- swapnodesinpairs24.py
+++ swapnodesinpairs24.py
@@ -6,4 +6,21 @@
 
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        
+        dummy = ListNode(0)
+        dummy.next = head
+        nodeLead = dummy
+        while nodeLead:
+            node1 = nodeLead.next
+            if node1:
+                node2 = node1.next
+                if node2:
+                    node3 = node2.next
+                    nodeLead.next = node2
+                    node2.next = node1
+                    node1.next = node3
+                    nodeLead = node1
+                else:
+                    break
+            else:
+                break
+        return dummy.next
