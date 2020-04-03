f628d09f7db9d686308e34eff64915ed7a9a0bba
blueones <yiq.shang@gmail.com>
2020-04-03 09:15:47 -0700

    maximum subarray

--- MaximumSub.py
+++ MaximumSub.py
@@ -1,8 +1,8 @@
 import math
 
-class Solution(object):
+class Solution2:
    
-        
+#Aug, 2019 divide and conquer.      
         
     def maxSub(self,nums):
         if len(nums)==0:
@@ -28,3 +28,34 @@ class Solution(object):
         return s
 
 print(Solution().maxSubArray([2,3,5,12,5,7,-2.-4]))
+class Solution:
+    #LC weekly challenge brute force
+    def maxSubArray(self, nums: List[int]) -> int:
+        len_num = len(nums)
+        left = 0
+        right = len_num-1
+        max_sum = float("-inf")
+        for start in range(len_num):
+            for end in range(start, len_num):
+                sum_current = sum(nums[start:end+1])
+                max_sum = max(max_sum,sum_current)
+        return max_sum
+class Solution1:
+    #O(N)solution, good intuitive solution
+    def maxSubArray(self, nums: List[int]) -> int:
+        len_num = len(nums)
+        sum_max = float("-inf")
+        current_sum = 0
+        for i in range(len_num):
+            current_sum += nums[i]
+            sum_max = max(sum_max,current_sum)
+            if current_sum<= 0:
+                current_sum = 0
+            
+        return sum_max
+class Solution3:
+    #divide and conquer from April 2020
+    def maxSubArray(self,nums):
+        
+
+        
