c8099af71fd462be11d4886c5d651b30aa5d409c
blueones <yiq.shang@gmail.com>
2020-04-11 16:18:14 -0700

    simplify path

--- simplifypath71.py
+++ simplifypath71.py
@@ -15,4 +15,18 @@ class Solution:
             result_string+="/"+stack_levels[0]
             stack_levels.pop(0)
         return result_string if result_string !="" else "/"
-            
+class Solution1:
+    def simplifyPath(self, path: str) -> str:
+        #solution from LC
+        list_levels = path.split("/")
+        stack_levels = []
+        for item in list_levels:
+            if item =="..":
+                if stack_levels:
+                    stack_levels.pop(-1)
+            elif item =="." or item=="":
+                continue
+            else:
+                stack_levels.append(item)
+        return "/"+"/".join(stack_levels)
+                      
