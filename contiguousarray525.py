ac4997e59f928ed163ea14a87bd8d4d3475b6db7
blueones <yiq.shang@gmail.com>
2020-04-17 21:58:43 -0700

    contiguous array review

--- contiguousarray525.py
+++ contiguousarray525.py
@@ -20,5 +20,24 @@ class Solution:
         print(count_dict)
         return max_con
         
+class Solution1:
+    def findMaxLength(self, nums: List[int]) -> int:
+        #no need to save the latter index as you did in solution.
+        count= 0
+        count_dict = {0:-1}
+        max_con = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count -= 1
+            elif nums[i] == 1:
+                count +=1
+            if count in count_dict:
+                
+                max_con = max(max_con,i- count_dict[count])
+            else:
+                count_dict[count]= i
+        return max_con
+        
 
+        
         
