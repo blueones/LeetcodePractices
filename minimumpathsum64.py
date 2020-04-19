7083cc15cfd4457069d26db0b64d5a647122f5e6
blueones <yiq.shang@gmail.com>
2020-04-19 09:11:20 -0700

    min path sum 64

---
+++ minimumpathsum64.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minPathSum(self, grid):
+        #recursive solution without DP
+        if grid == []:
+            return 0
+        row = len(grid)
+        column = len(grid[0])
+        self.min_sum = float("inf")
+        
+        def helper(row_index,column_index, current_sum):
+            if row_index == row-1 and column_index == column-1:
+                self.min_sum = min(self.min_sum,(current_sum+grid[row_index][column_index]))
+            else:
+                if row_index+1<row:
+                    helper(row_index+1,column_index,current_sum+ grid[row_index][column_index])
+                if column_index+1<column:
+                    helper(row_index,column_index+1, current_sum+grid[row_index][column_index])
+        helper(0,0,0)
+        return self.min_sum
+class Solution1:
+    def minPathSum(self,grid):
+        #DP
+        if grid == []:
+            return 0
+        row = len(grid)
+        column = len(grid[0])
+        dp = [[float("inf") for i in range(column)] for j in range(row)]
+        dp[0][0] = grid[0][0]
+        for i in range(row):
+            for j in range(column):
+                if i-1>=0:
+                    dp[i][j]= min(dp[i][j],dp[i-1][j]+grid[i][j])
+                if j-1>=0:
+                    dp[i][j]= min(dp[i][j],dp[i][j-1]+grid[i][j])
+        return dp[row-1][column-1]
+Solution1().minPathSum([[1,3,1],[1,5,1],[4,2,1]])
