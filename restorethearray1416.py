40c0d4d29fa10080af18154a8f8fe123f1792775
blueones <yiq.shang@gmail.com>
2020-04-19 18:55:18 -0700

    restore the array DP

--- restorethearray1416.py
+++ restorethearray1416.py
@@ -21,7 +21,23 @@ class Solution:
         if s == "":
             return 0
         return helper(s,k)
-class Solution:
-    def num(self,s,k):
-        def digitsNum(num):
-            return len(str(num))
+class Solution1:
+    def numberOfArrays(self, s, k):
+        #DP
+        kMod = 10**9+7 #avoid overflow. 
+        dp = [-1 for i in range(len(s)+1)] # dp has len(s)+1 slots. 0~ len(s). dp[len(s)] is saying that at the right of the last digit, there is 1 "".
+        dp[len(s)] = 1
+        for i in range(len(s)-1,-1, -1):
+            if s[i] == "0":
+                continue
+            dp[i] = 0
+            for j in range(i+1, len(s)+1, 1): # thus we need to loop till j == len(s).
+                if int(s[i:j]) <= k:
+                    if dp[j]!= -1:
+                        
+                        dp[i] = (dp[i] + dp[j]) % kMod 
+                else:
+                    break
+        return dp[0]
+print(Solution1().numberOfArrays("600342244431311113256628376226052681059918526204",703))
+
