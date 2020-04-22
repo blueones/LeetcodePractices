29cc0fdd97e1b98f34f6e4215204673e2cb82b2c
blueones <yiq.shang@gmail.com>
2020-04-21 20:12:03 -0700

    LC challenge O(n+m) solution

--- leftmostcolumnwithatleastaone.py
+++ leftmostcolumnwithatleastaone.py
@@ -1,6 +1,14 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
+
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
-        #nlogn solution
+        #nlogn solution, binary search
         row, column = binaryMatrix.dimensions()
         
         def helper(checked_column):
@@ -20,4 +28,23 @@ class Solution:
                 start = mid+1
                 
         return start if start < column else -1
-            
+
+class Solution1:
+    def leftMostColumnWithOne(self,binaryMatrix):
+         #use a pointer 
+         # O(n+m)
+        row, column = binaryMatrix.dimensions()
+        pointer_row = 0
+        pointer_column = column
+        while pointer_row < row or pointer_column > 0:
+            if binaryMatrix.get(pointer_row, pointer_column-1) == 1:
+                if pointer_column-1 == 0:
+                    return 0
+                else:
+                    pointer_column -= 1
+            else:
+                if pointer_row == row-1:
+                    break
+                else:
+                    pointer_row += 1
+        return pointer_column if pointer_column != column else -1
