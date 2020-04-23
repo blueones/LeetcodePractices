71e7d67506c20d9ffe82d7581017a944b3519c96
blueones <yiq.shang@gmail.com>
2020-04-22 22:04:25 -0700

    DP 2D

---
+++ addstrings415.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        def toInteger(input_string):
+            res = 0
+            for cha in input_string:
+                res = res*10+int(cha)
+            return res
+        def toString(input_integer):
+            res = ""
+            while input_integer>0:
+                remainder = str(input_integer%10)
+                res = remainder + res
+                input_integer = input_integer//10
+            return res if res!="" else "0"
+        
+        return toString(toInteger(num1)+toInteger(num2))
--- deletiondistance.py
+++ deletiondistance.py
@@ -1,16 +1,19 @@
-class Soltuion:
+class Solution:
     def deletion_distance(self,str1,str2):
         len1 = len(str1)
-def deletion_distance(str1, str2):
-  len1 = len(str1)
-  len2 = len(str2)
-  result_max = 0
-  def helper_function(index1,index2,same_sofar,result_max):
-    if index1==len1 or index2 ==len2:
-      result_max = max(result_max,same_sofar)
-      return result_max
-    for ind1 in range(index1,len1):
-      for ind2 in range(index2,len2):
-        if str1[ind1]==str2[ind2]:
-          return helper_function(ind1+1,ind2+1,same_sofar+1,result_max)
-  return len1+len2-2*helper_function(0,0,0,result_max)
+        len2 = len(str2)
+        dp = [[0 for i in range(len2+1)] for j in range(len1+1)]
+        ind1 = 0
+        for ind2 in range(0, len2+1): 
+            dp[ind1][ind2] = ind2
+        ind2 = 0
+        for ind1 in range(0,len1+1):
+            dp[ind1][ind2] = ind1
+        for ind1 in range(1,len1+1):
+            for ind2 in range(1, len2+1):
+                if str1[ind1-1] == str2[ind2-1]:
+                    dp[ind1][ind2] = dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2] = min(dp[ind1][ind2-1]+1, dp[ind1-1][ind2]+1)
+        return dp[len1][len2]
+print(Solution().deletion_distance("","hit"))
