0bb8488796fd72290d8c52e3cff289ce16ade516
blueones <yiq.shang@gmail.com>
2020-05-06 23:40:12 -0700

    count primes

---
+++ countrpimes204.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countPrimes(self, n):
+        def prime(number):
+            for num in range(2, math.floor(sqrt(number))+1):
+                if number%num == 0:
+                    return False
+            return True
+        counter = 0
+        for i in range(2, n):
+            if prime(i):
+                counter += 1
+        return counter
+
+class Solution1:
+    def countPrimes(self, n):
+        #Sieve of Eratosthenes
+        def prime(number):
+            for num in range(2, math.floor(sqrt(number))+1):
+                if number%num == 0:
+                    return False
+            return True
+        marked = [True for i in range(n)]
+        if n < 2:
+            return 0
+        marked[0] = marked[1] = False
+        
+        for i in range(2, n):
+            if marked[i] == True:
+                if prime(i):
+                    times = 2
+                    current = i*times
+                    while current < n:
+                        marked[current] = False
+                        times += 1
+                        current = i*times
+        return sum(1 for i in marked if i == True)
+
+                
+
