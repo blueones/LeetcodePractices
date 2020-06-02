b4ff116b630f3ea3aeb8553197b68f3c8fbeed9d
blueones <yiq.shang@gmail.com>
2020-06-01 22:16:48 -0700

    quickfind

--- kthclosestpointstoorigin973.py
+++ kthclosestpointstoorigin973.py
@@ -18,4 +18,28 @@ class Solution1:
         
 class Solution2:
     def kClosest(self, points, K):
-        #
+        #quickfind
+        def quick_find(start, end):
+            end_pivot = points[end]
+            pivot_distance = end_pivot[0]**2+end_pivot[1]**2
+            pivot = start
+            for i in range(start, end, 1):
+                if points[i][0]**2+points[i][1]**2 <= pivot_distance:
+                    points[pivot], points[i] = points[i], points[pivot]
+                    pivot += 1
+            points[pivot], points[end] = points[end], points[pivot]
+            return pivot
+                    
+                    
+        def findKth(start, end):
+            pivot = quick_find(start, end)
+            if pivot == K - 1:
+                print(pivot)
+                return pivot
+            elif pivot < K-1:
+                return findKth(pivot+1, end)
+            else:
+                return findKth(start, pivot-1)
+        pivot = findKth(0, len(points)-1)
+        ans = points[:pivot+1]
+        return ans
