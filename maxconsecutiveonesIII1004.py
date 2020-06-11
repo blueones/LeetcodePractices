d66620564ce77c5b624d438ad707cc03177ce877
blueones <yiq.shang@gmail.com>
2020-06-11 08:32:44 -0700

    sliding window

---
+++ maxconsecutiveonesIII1004.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestOnes(self, A: List[int], K: int) -> int:
+        #invariable is max number of K zeros in window.
+        start = 0
+        max_substring = 0
+        len_string = len(A)
+        counter = 0
+        zeros = 0
+        index = 0
+        while index < len_string:
+            
+            if A[index]== 1:
+                counter += 1
+                
+                max_substring = max(max_substring, counter)
+            else:
+                if zeros == K:
+                    while A[start]!= 0:
+                        start += 1
+                        counter -= 1
+                    start += 1
+                    
+                else:
+                    zeros += 1
+                    counter += 1
+            index += 1
+        return max_substring
