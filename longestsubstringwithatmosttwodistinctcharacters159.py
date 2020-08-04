42a535e90d3d022cca94b285e663bf509e74c400
blueones <yiq.shang@gmail.com>
2020-08-03 21:48:53 -0700

    sliding window

--- longestsubstringwithatmosttwodistinctcharacters159.py
+++ longestsubstringwithatmosttwodistinctcharacters159.py
@@ -19,3 +19,19 @@ class Solution:
                 visited[cha] = 1
             max_length = max(max_length, len(queue))
         return max_length
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        covered = defaultdict()
+        left = right = 0
+        max_length = 0
+        while right < len(s):
+            
+            if s[right] not in covered and len(covered) == 2:
+                covered_list = sorted(covered, key = lambda i:covered[i])
+                topop = covered_list[0]
+                left = covered[topop]+1
+                covered.pop(topop)
+            covered[s[right]] = right
+            max_length = max(max_length, right-left+1)
+            right += 1
+        return max_length
